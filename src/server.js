36e631dff481c26b90a095ae3b338aac3d5e83c5
dougras <emaildedouglas@gmail.com>
2025-02-27 21:59:06 -0300

    ajuste server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 80;
+// Determinar o diretório base corretamente
+const distDir = path.join(__dirname, '..', 'dist', 'turma-lina', 'browser');
+console.log('Servindo arquivos de:', distDir);
 
-app.use(express.static(__dirname + '/dist/turma-lina/browser'))
+// Servir arquivos estáticos do Angular
+app.use(express.static(distDir));
 
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/dist/turma-lina/browser/index.csr.html');
+// Roteamento para a aplicação Angular (Single Page Application - SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distDir, 'index.csr.html'));
 });
 
-app.listen(PORT, () => {
-    console.log('Servidor iniciado na porta ' + PORT)
-})
+// Definir a porta
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log(distDir);
+    
+  console.log(`Servidor rodando na porta ${port}`);
+});
